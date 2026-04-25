e0ef0c342acd2ad68ce0a98f7bb04a114f553c08
rachnarajaka040 <rachnarajak040@gmail.com>
2026-04-25 18:25:44 +0530

    add

--- src/Com/LandingPage.jsx
+++ src/Com/LandingPage.jsx
@@ -5,7 +5,7 @@ function LandingPage() {
         <div class="container mt-5 d-flex justify-content-center">
             <div class="card p-3" style={{ width: "500px", height: "600px", border: "2px solid black" }}>
                 <div style={{ marginTop: "50%" }}>
-                    <h1>This is my Mern Project</h1>
+                    <h1>This is my rachna rajak</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, doloremque.</p>
                     <div class="d-grid gap-2 col-2 mx-auto" style={{ width: "18rem" }}>
                         <Link to="/signup"><button class="btn btn-primary w-100" type="button">Create Account</button></Link>
