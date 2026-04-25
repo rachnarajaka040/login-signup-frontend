8c0826922586cabc80788cb8fd103ab12adcaa69
rachnarajaka040 <rachnarajak040@gmail.com>
2026-04-25 18:52:12 +0530

    add

--- src/Com/Login.jsx
+++ src/Com/Login.jsx
@@ -23,7 +23,7 @@ function Login() {
                     <div class="col-lg-4 col-md-6 col-sm-6">
                         <div class="card shadow">
                             <div class="card-title text-center border-bottom">
-                                <h2 class="p-3">Login to Your Popx account</h2>
+                                <h2 class="p-3">Login to Your jenkins account</h2>
                             </div>
                             <div class="card-body">
                                 <form onSubmit={handleSubmit}>
